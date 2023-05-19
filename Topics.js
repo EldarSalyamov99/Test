390660cea6609b320e191dfa00d4a10a020fb696
MarineMi <m.g.vasyukova@gmail.com>
2023-05-19 11:19:47 +0300

    release - class create

---
+++ Topics.js
@@ -0,0 +1,8 @@
+
+
+class Topics {
+    constructor (args = {}) {
+        this.question= args.question
+        this.answer = args.answer
+    }
+}
---
+++ User.js
@@ -0,0 +1,6 @@
+class User {
+    constructor (name = '', scores = 0) {
+        this.name = name
+        this.scores = scores
+    }
+}
