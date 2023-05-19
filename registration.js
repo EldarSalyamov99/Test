0df66e91bda1f65c5920371f3cbac99d0c091690
Ilia <iliya.britvin@gmail.com>
2023-05-19 12:38:36 +0300

    registration edit1

--- registration.js
+++ registration.js
@@ -3,11 +3,11 @@ const User = require('./User');
 
 function registrateUser() {
   console.clear();
-  const userName = readlineSync.question('Введи своё имя: ');
+  const userName = readlineSync.question('Введите своё имя: ');
   const newUser = new User(userName);
   console.clear();
-  const chosenTheme = readlineSync.question(
-    `Привет, ${newUser.name}! Выбери тему:
+  let chosenTheme = readlineSync.question(
+    `Здравствуйте, ${newUser.name}! Выберите номер темы:
 
     1. Nighthawk
     2. Otter
@@ -15,5 +15,25 @@ function registrateUser() {
     
     `
   );
+
+  console.clear();
+
+  switch (chosenTheme) {
+    case '1':
+      chosenTheme = './topics/nighthawk_flashcard_data.txt';
+      break;
+
+    case '2':
+      chosenTheme = './topics/otter_flashcard_data.txt';
+      break;
+
+    case '3':
+      chosenTheme = './topics/raccoon_flashcard_data.txt';
+      break;
+
+    default:
+      console.log(`Темы под номером "${chosenTheme}" не существует!`);
+      break;
+  }
 }
 registrateUser();
