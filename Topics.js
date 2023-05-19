94cb04bdabf2b1af1913593e9690cc10b57a3ca5
MarineMi <m.g.vasyukova@gmail.com>
2023-05-19 12:39:52 +0300

    read txt & create array themes

--- Topics.js
+++ Topics.js
@@ -1,8 +1,36 @@
+const fs = require('fs');
 
+// class Topic {
+//   constructor(args = {}) {
+//     this.question = args.question;
+//     this.answer = args.answer;
+//   }
+// }
 
-class Topics {
-    constructor (args = {}) {
-        this.question= args.question
-        this.answer = args.answer
-    }
+// const nighthawk = new Topic()
+
+const nighthawkArr = fs
+  .readFileSync(`${__dirname}/topics/nighthawk_flashcard_data.txt`, 'utf-8')
+  .split('\n\n');
+const nighthawk = [];
+for (let i = 0; i < nighthawkArr.length; i += 1) {
+  nighthawk.push(nighthawkArr[i].trim().split('\n'));
 }
+
+const otterArr = fs
+  .readFileSync(`${__dirname}/topics/otter_flashcard_data.txt`, 'utf-8')
+  .split('\n\n');
+const otter = [];
+for (let i = 0; i < otterArr.length; i += 1) {
+  otter.push(otterArr[i].trim().split('\n'));
+}
+
+const raccoonArr = fs
+  .readFileSync(`${__dirname}/topics/raccoon_flashcard_data.txt`, 'utf-8')
+  .split('\n\n');
+const raccoon = [];
+for (let i = 0; i < raccoonArr.length; i += 1) {
+  raccoon.push(raccoonArr[i].trim().split('\n'));
+}
+
+console.log(nighthawk, otter, raccoon);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const otterArr = fs.readFileSync(`${__dirname}/topics/otter_flashcard_data.txt`, 'utf-8').split('\n\n');
+// const otter = []
+// for (let i = 0; i < otterArr.length; i++){
+//     otter.push(otterArr[i].split('\n'))
+// }
